1661c58ae86f49ed51c961a8ae3a6b1b4d20c1e0
Aarruran S <108710414+AARRU007@users.noreply.github.com>
2023-09-01 10:19:24 +0530

    Add files via upload

---
+++ Lagger (1)/Lagger/src/main/java/com/example/Lagger/LaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaggerApplication.class, args);
+	}
+
+}
---
+++ Lagger (1)/Lagger/src/test/java/com/example/Lagger/LaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
